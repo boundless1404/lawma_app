d29338cc34c8514bce53e732d9d5c34d380bf9d5
AkoniHakeem <akozymayor@gmail.com>
2024-06-02 21:47:04 +0100

    updated build files

---
+++ dist/spa/assets/AuthLayout.49900fbf.js
@@ -0,0 +1,1 @@
+import{Q as o,a as r}from"./QLayout.bc22860c.js";import{cJ as _,aY as s,aQ as c,ac as n,bj as e,f as a}from"./index.ed6ac60f.js";import"./QResizeObserver.5deb0f27.js";const u={};function f(i,p){const t=s("router-view");return c(),n(o,{view:"hHh lpR fFf"},{default:e(()=>[a(r,{class:"bg-primary"},{default:e(()=>[a(t)]),_:1})]),_:1})}var d=_(u,[["render",f]]);export{d as default};
