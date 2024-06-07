cfd8eaa480c38a86add98fe38986365913a1bee1
AkoniHakeem <akozymayor@gmail.com>
2024-06-07 17:43:14 +0100

    rebuilt the code

---
+++ dist/spa/assets/AuthLayout.68e191c1.js
@@ -0,0 +1,1 @@
+import{Q as o,a as r}from"./QLayout.7aa86820.js";import{cJ as _,aY as s,aQ as c,ac as n,bj as e,f as a}from"./index.6e5bb15d.js";import"./QResizeObserver.49d80734.js";const u={};function f(i,p){const t=s("router-view");return c(),n(o,{view:"hHh lpR fFf"},{default:e(()=>[a(r,{class:"bg-primary"},{default:e(()=>[a(t)]),_:1})]),_:1})}var d=_(u,[["render",f]]);export{d as default};
