6fee1d90f22737010089e0f9a85b64fcefe96dc0
AkoniHakeem <akozymayor@gmail.com>
2024-07-25 15:54:22 +0100

    removed dist

--- dist/spa/assets/AuthLayout.9da47212.js
+++
@@ -1,1 +0,0 @@
-import{Q as o,a as r}from"./QLayout.6ada0e98.js";import{cJ as _,aY as s,aQ as c,ac as n,bj as e,f as a}from"./index.1a32d67c.js";import"./QResizeObserver.064d12f3.js";const u={};function f(i,p){const t=s("router-view");return c(),n(o,{view:"hHh lpR fFf"},{default:e(()=>[a(r,{class:"bg-primary"},{default:e(()=>[a(t)]),_:1})]),_:1})}var d=_(u,[["render",f]]);export{d as default};
