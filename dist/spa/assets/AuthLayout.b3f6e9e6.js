a6c28fe64171a1b84ec1060c96d2de51ef7549ea
akonihakeem <akozymayor@gmail.com>
2024-11-02 18:12:13 +0100

    generated new dist

---
+++ dist/spa/assets/AuthLayout.b3f6e9e6.js
@@ -0,0 +1,1 @@
+import{Q as o,a as r}from"./QLayout.0842ea83.js";import{cJ as _,aY as s,aQ as c,ac as n,bj as e,f as a}from"./index.49c4ee4f.js";import"./QResizeObserver.a5f4c9e8.js";const u={};function f(i,p){const t=s("router-view");return c(),n(o,{view:"hHh lpR fFf"},{default:e(()=>[a(r,{class:"bg-primary"},{default:e(()=>[a(t)]),_:1})]),_:1})}var d=_(u,[["render",f]]);export{d as default};
